746bdd3c45173e67669273c5fad972f4d2e50ea9
mewcoder <1010953107@qq.com>
2022-07-03 15:49:07 +0800

    feat: 🎸 修改函数类型

--- solutions/javascript/原型相关/index.js
+++ solutions/javascript/原型相关/index.js
@@ -11,7 +11,7 @@ const isFunction = (obj) => {
  * @param  {...any} args
  * @returns object
  */
-export const _new = (Fn, ...args) => {
+export function _new(Fn, ...args) {
   if (!isFunction(Fn)) {
     throw new TypeError("Fn must be a function!");
   }
@@ -20,15 +20,15 @@ export const _new = (Fn, ...args) => {
   // 前两行可以换成 obj = Object.create(Fn.prototype)
   const res = Fn.apply(obj, args);
   return isObject(res) || isFunction(res) ? res : obj;
-};
+}
 
 /**
  *
  * @param {*} obj
  * @returns obj
  */
-export const create = (obj) => {
+export function create(obj) {
   function Fn() {}
   Fn.prototype = obj;
   return new Fn();
-};
+}
